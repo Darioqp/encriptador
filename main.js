91c98bbe15ae8ba54f72a09cd157a57cf61f150e
Dario Kupec <dariokupec@gmail.com>
2023-01-20 16:05:51 -0400

    Se creo la funcion que valida que no haya mayúsculas ni acentos

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-let texto = document.getElementById('caja-texto').value;
-console.log(texto);
+function validarTexto() {
+  let texto = document.getElementById('caja-texto').value;
+  const regex1 = /[A-Z]/;
+  const regex2 = /[(\u00C0-\u017F\)]/;
+
+//   console.log(regex.test(texto));
+
+
+//   console.log(texto);
+
+  if (regex1.test(texto)) {
+    alert("¡Debes ingresar el texto en minúsculas!");
+  } else if (regex2.test(texto)){
+    alert("¡No se pueden incluir acentos en el texto!")
+  } else {
+    console.log("Texto validado")
+  }
+}
