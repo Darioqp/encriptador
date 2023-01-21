9c83ab8f84aac495a6d76f90558652366436ca74
Dario Kupec <dariokupec@gmail.com>
2023-01-20 21:58:54 -0400

    Funciona el boton de encriptar y se imprime el resultado en la consola del navegador

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
-function validarTexto() {
-  let texto = document.getElementById('caja-texto').value;
-  const regex1 = /[A-Z]/;
-  const regex2 = /[(\u00C0-\u017F\)]/;
+function encriptarTexto() {
+  let texto = document.getElementById("caja-texto").value;
+  const regex = /[^a-z0-9\s]/;
+  console.log(regex.test(texto));
 
-//   console.log(regex.test(texto));
-
-
-//   console.log(texto);
-
-  if (regex1.test(texto)) {
-    alert("¡Debes ingresar el texto en minúsculas!");
-  } else if (regex2.test(texto)){
-    alert("¡No se pueden incluir acentos en el texto!")
+  if (regex.test(texto)) {
+    alert("¡No se permiten letras mayúsculas ni caracteres especiales!");
   } else {
-    console.log("Texto validado")
+    let textoEncriptado = texto.replace(/a|e|i|o|u/g, function(letra) {
+      if (letra === "a") {
+        return "ai";
+      } else if (letra === "e") {
+        return "enter";
+      } else if (letra === "i") {
+        return "imes";
+      } else if (letra === "o") {
+        return "ober";
+      } else if (letra === "u") {
+        return "ufat";
+      }
+    })
+    console.log(textoEncriptado);
   }
 }
+
+function desencriptarTexto() {
+  
+}
+
+//código separado en dos funciones
+
+// function validarTexto(texto) {
+//   const regex = /[^a-z0-9\s]/;
+//   return !regex.test(texto);
+// }
+
+// function encriptarTexto() {
+//   let texto = document.getElementById("caja-texto").value;
+
+//   if (!validarTexto(texto)) {
+//     alert("¡No se permiten letras mayúsculas ni caracteres especiales!");
+//   } else {
+//     let textoEncriptado = texto.replace(/a|e|i|o|u/gi, function(letra) {
+//         switch (letra) {
+//           case "a":
+//             return "ai";
+//           case "e":
+//             return "enter";
+//           case "i":
+//             return "imes";
+//           case "o":
+//             return "ober";
+//           case "u":
+//             return "ufat";
+//         }
+//     });
+//     console.log(textoEncriptado);
+//   }
+// }
